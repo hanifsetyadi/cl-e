1772dd9b7a5d3e3fae0376b1a739c45fafdaa108
hanifsetyadi <hfsetyadi21@gmail.com>
2024-05-13 13:11:24 +0700

    commit

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
-// Menggunakan middleware untuk menangani permintaan untuk file statis
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Mengirimkan index.html saat permintaan GET '/'
 app.get('/', (req, res) => {
     res.render('./public/index.html')
 });
